6149af204cd93d159a4c12a0abd7c8402d650295
AlexisVillarruelEng <alexisvillarruelj14@gmail.com>
2025-05-23 15:55:17 -0500

    se corrigio la inyeccion de dependencias

--- app/src/main/java/dev/alexisvillarruel/imageapp/service/RetrofitInstance.kt
+++ app/src/main/java/dev/alexisvillarruel/imageapp/service/RetrofitInstance.kt
@@ -5,23 +5,21 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.alexisvillarruel.imageapp.ui.splashscreen.data.network.SplashClient
-import jakarta.inject.Singleton
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RetrofitInstance {
-
-
-    private val baseURL = "https://api.unsplash.com/"
-    private val APIKEY="Z8TttGF5-S2zB0D85_yNMCAl8n666BRTtNlN2qbHnjw"
-    private val client = OkHttpClient.Builder().addInterceptor(AuthInterceptor(APIKEY)).build() // con authinterceptor
-
+    //no inicializar nada aqui
     @Singleton
     @Provides
     fun provideRetrofit(): Retrofit{
+         val baseURL = "https://api.unsplash.com/"
+         val APIKEY="Z8TttGF5-S2zB0D85_yNMCAl8n666BRTtNlN2qbHnjw"
+         val client = OkHttpClient.Builder().addInterceptor(AuthInterceptor(APIKEY)).build() // con authinterceptor
         return Retrofit.Builder()
             .baseUrl(baseURL)
             .addConverterFactory(GsonConverterFactory.create())
