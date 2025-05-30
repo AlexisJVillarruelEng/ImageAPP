a3b001d8c58cce0c369083effeba8078eb729d85
Alexis Jhair <164811242+AlexisJVillarruelEng@users.noreply.github.com>
2025-05-29 20:41:49 -0500

    añadio pantalla Registerform.kt

--- app/src/main/java/dev/alexisvillarruel/imageapp/ui/login/ui/components/registerc/Registerform.kt
+++ app/src/main/java/dev/alexisvillarruel/imageapp/ui/login/ui/components/registerc/Registerform.kt
@@ -1,11 +1,15 @@
 package dev.alexisvillarruel.imageapp.ui.login.ui.components.registerc
 
+import android.widget.ImageButton
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -15,11 +19,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.alexisvillarruel.imageapp.R
 import dev.alexisvillarruel.imageapp.ui.theme.AppTheme
 
 @Composable
@@ -30,29 +38,48 @@ fun registerform() {
                 Text(
                     "Register Form", style = MaterialTheme.typography.displayLarge,
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxWidth().align(Alignment.CenterHorizontally)
                         .paddingFromBaseline(
                             top = 16.dp,
                             bottom = 16.dp
-                        ) // Padding from baseline for better spacing
+                        ),
+                    textAlign = TextAlign.Center
                 )
                 OutlinedTextField(
                     value = "",
                     onValueChange = {},
-                    modifier = Modifier
+                    label = { Text("Email") },
+                    modifier = Modifier.padding(16.dp)
                         .fillMaxWidth(),
                 )
                 OutlinedTextField(
                     value = "",
                     onValueChange = {},
-                    modifier = Modifier
+                    label = { Text("nombre") },
+                    modifier = Modifier.padding(16.dp)
                         .fillMaxWidth(),
                 )
                 OutlinedTextField(
                     value = "",
                     onValueChange = {},
-                    modifier = Modifier
+                    label = { Text("contraseña") },
+                    modifier = Modifier.padding(16.dp)
                         .fillMaxWidth(),
+                )
+                Text("Click en logo para registrar",modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 4.dp, top = 4.dp),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Image(
+                    painter = painterResource(id =
+                        R.drawable.logo), // Replace with your drawable resource
+                    contentDescription = "Register Icon",
+                    modifier = Modifier
+                        .padding(16.dp).fillMaxWidth()
+                        .size(50.dp)
+                        .clickable { /* Handle click */ },
+                    alignment = Alignment.Center
                 )
 
             }
